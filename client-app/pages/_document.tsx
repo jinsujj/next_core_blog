7745875781dbf0591736d45ae26337f9dc41d78a
jinsujj <wlstncjs1234@naver.com>
2022-05-15 18:48:55 +0900

    Google Analytics  등록

--- client-app/pages/_document.tsx
+++ client-app/pages/_document.tsx
@@ -5,7 +5,7 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document";
-
+import { GA_TRACKING_ID } from "../utils/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -18,29 +18,65 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* favIcon */}
-          <link rel="shortcut icon" type="image/x-icon" href="./img/owl.svg"/>
-          <link rel="icon" href="./img/owl.svg"/>
-          <link rel="apple-touch-icon" href="./img/owl.svg"/>
+          <link rel="shortcut icon" type="image/x-icon" href="./img/owl.svg" />
+          <link rel="icon" href="./img/owl.svg" />
+          <link rel="apple-touch-icon" href="./img/owl.svg" />
 
           {/* Standard Font */}
-          <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic+Coding:400" rel="stylesheet"/>
-          <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic:400" rel="stylesheet"></link>
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600" rel="stylesheet"/>
-          <link href="https://hangeul.pstatic.net/hangeul_static/css/NanumHimNaeRaNeunMarBoDan.css" rel="stylesheet"/>
-          <link href="https://hangeul.pstatic.net/hangeul_static/css/NanumBaReunHiPi.css" rel="stylesheet"></link>
+          <link
+            href="https://fonts.googleapis.com/css?family=Nanum+Gothic+Coding:400"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Nanum+Gothic:400"
+            rel="stylesheet"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600"
+            rel="stylesheet"
+          />
+          <link
+            href="https://hangeul.pstatic.net/hangeul_static/css/NanumHimNaeRaNeunMarBoDan.css"
+            rel="stylesheet"
+          />
+          <link
+            href="https://hangeul.pstatic.net/hangeul_static/css/NanumBaReunHiPi.css"
+            rel="stylesheet"
+          ></link>
 
-          
           {/* jquery 3.6.0 */}
           <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
           {/* bootstrap  3.4.1*/}
           <link
-            href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet"/>
+            href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+            rel="stylesheet"
+          />
           <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
           {/* summmernote 0.8.20 */}
-          <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet"/>
+          <link
+            href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css"
+            rel="stylesheet"
+          />
           <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });`,
+            }}
+          />
         </Head>
         <body>
           <Main />
