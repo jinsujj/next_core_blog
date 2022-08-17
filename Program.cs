303bb3ed950ec6b76e7fe256375e674d49900d68
jinsu-jj <wlstncjs1234@naver.com>
2022-08-17 20:28:15 +0900

    .net 6 migration

--- Program.cs
+++ Program.cs
@@ -1,26 +1,100 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Next_Core_Blog.Context;
+using Next_Core_Blog.Repository.BlogNote;
+using Next_Core_Blog.Repository.Users;
 
-namespace Next_Core_Blog
+
+var builder = WebApplication.CreateBuilder(args);
+
+/*
+*  'builder.Services' gets called by the runtime. Use this method to add services to the container.
+*/
+builder.Services.AddControllers();
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+// Controller loop Handling
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// Cors Setting
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Dev", builder =>
+    {
+        builder.WithMethods("GET", "POST", "PATCH", "DELETE")
+        .AllowCredentials()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed(origin =>
+        {
+            if (string.IsNullOrWhiteSpace(origin)) return false;
+            if (origin.ToLower().StartsWith("https://localhost")) return true;
+            if (origin.ToLower().StartsWith("https://owl-dev.me")) return true;
+            if (origin.ToLower().StartsWith("https://www.owl-dev.me")) return true;
+            return false;
+        });
+    });
+});
+
+// Cookie Setting
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.Cookie.Name = "UserLoginCookie";
+        options.SlidingExpiration = true;
+        options.ExpireTimeSpan = new TimeSpan(6, 0, 0); // 6 hour
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.None;
+        options.Cookie.Domain =".owl-dev.me";
+    });
+
+// Swagger Generateç
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Next_Core_Blog", Version = "v1" });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Next_Core_Blog v1"));
 }
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+// static file dir use wwwroot
+app.UseStaticFiles();
+
+// useCors should be located between useRouting and useAuthorization
+app.UseCors("Dev");
+app.UseAuthentication();
+app.UseAuthentication();
+
+// Nginx Reverse Proxy - Real Ip Set
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
